07646f86ef05edf875b826b07ae2b8693307be7f
Maria-LouizaVdB <marialouiza.vandenbergh@gmail.com>
2026-03-18 09:35:02 +0000

    add max iteration to via function

--- via_function.py
+++ via_function.py
@@ -6,8 +6,9 @@ def value_iteration_function(states: List[Any],
                              trans_fun: Callable[ [Any,Any,Any], float ],  
                              reward_fun: Callable[ [Any,Any,Any], float ],
                              epsilon: float = 0.05,
-                             gamma: float = 0.95
-                            ) -> tuple[dict, dict]:
+                             gamma: float = 0.95,
+                             max_iteration: int = 100
+                            ) -> tuple[dict, dict, int]:
 
     """ 
     Perform value interation
@@ -17,18 +18,22 @@ def value_iteration_function(states: List[Any],
         actions - list of actions between states
         trans_fun - function that inputs 2 states and an action and returns probability that the action occurs 
         reward_fun - function that takes in 2 states and an action, and returns a reward
-        epsilon - treshold to check if value function has stabilised
-        gamma - discount factor
+        gamma - discount factor, default 0.95
+        epsilon - treshold to check if value function has stabilised, default 0.05
+        max_iteration - maximum number of iterations before the function ends to avoid infinite loop, default 100
 
     output:
         pi - approximate optimal policy
         v - value function
+        k - number of iteration steps before convergence
     """
     
     # calculate value function values until stabalises
     v = {s: 0.0 for s in states}
-    
+    k = 0
+
     while True:
+        k += 1
         delta = 0
         # calculate value function for each state
         for s in states:
@@ -40,7 +45,10 @@ def value_iteration_function(states: List[Any],
         # check stabalised
         if delta < epsilon:
             break
-
+        # escape infinite loop
+        if k > max_iteration: 
+            return print("Warning: did not converge within", max_iteration, "iteration steps.")
+        
     # determine policy
     pi = {s: 0.0 for s in states}
     
@@ -49,4 +57,4 @@ def value_iteration_function(states: List[Any],
                                           trans_fun(s,s_next,a) * gamma * v[s_next] for s_next in states) for a in actions ] )]
         # numpy.argmax takes list input!
     
-    return v, pi
+    return v, pi, k
